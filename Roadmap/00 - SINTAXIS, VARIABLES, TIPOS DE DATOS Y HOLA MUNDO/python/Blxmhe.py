9d63c947e72466fc5c04f7606a3a2d0a5ac30618
Blxmhe <daniel.soto.ligena@gmail.com>
2024-11-09 19:43:28 -0300

    Create Blxmhe.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Blxmhe.py
@@ -0,0 +1,22 @@
+# https://www.python.org/
+
+# Comentario en una línea
+
+"""
+Este es un
+comentario
+en varias líneas
+"""
+
+'''
+Con comillas
+simples también
+sirve para hacer un
+comentario en varias
+lineas, usando Python
+'''
+
+my_variable = "Mi variable"
+my_variable = "Nuevo valor de mi variable"
+
+MY_CONSTANT = "Mi constante" #Por convención
